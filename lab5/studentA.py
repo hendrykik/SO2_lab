05c67d235a81a6c6d6c6aa9d37c8ad3c5b50a753
WigierSky <WigierSky@users.noreply.github.com>
2023-05-22 16:42:40 +0200

    Update

--- lab5/studentA.py
+++ lab5/studentA.py
@@ -2,32 +2,33 @@ def print_board(board):
     for i in range(3):
         i = i * 3
         print(board [i], " | ", board [i+1], " | ", board [i+2] )
-        if not i==5: print("--------------")
+        if not i==6: print("--------------")
+    print("\n\n")
 
 
 def is_game_over(board):
     j = 0
     for i in range(3):
-        if (board[j] != " " and board[j] == board[j+1] and board[j+1] == board[j+2]):
+        if (board[j] != ' ' and board[j] == board[j+1] and board[j+1] == board[j+2]):
             return True
         j = i + 3
-        if (board[i] != " " and board[i] == board[i+3] and board[i+3] == board[i+6]):
+        if (board[i] != ' ' and board[i] == board[i+3] and board[i+3] == board[i+6]):
             return True
-    if (board[0] != " " and board[0] == board[4] and board[4] == board[8]):
+    if (board[0] != ' ' and board[0] == board[4] and board[4] == board[8]):
         return True
-    if (board[2] != " " and board[2] == board[4] and board[4] == board[6]):
+    if (board[2] != ' ' and board[2] == board[4] and board[4] == board[6]):
         return True
 
     x = 0
     for i in range(9):
-        if ( board[i] != " "):
+        if ( board[i] != ' '):
             x = x + 1
         
-    if (x == 0): return True
+    if (x == 9): return True
 
     return False
 
 def new_board():
-    return [" " for _ in range(9)]
+    return [' ' for _ in range(9)]
 
 #def announce_outcome(board, players_move):
--- lab5/studentB.py
+++ lab5/studentB.py
@@ -1,17 +1,19 @@
 import random
 from studentA import is_game_over
 
-def get_usser_move(board):
+def get_user_move(board):
     choice = input("Your turn! Give number from 1 to 9: ")
-    while true:
-        if board[choice + 1] == ' ':
-            makeMove(board, "X", choice)
-            return
+    choice = int(choice)
+    while True:
+        if board[choice - 1] == ' ':
+            makeMove(board, 'X', choice - 1)
+            return board
         else:  
             choice = input("This place is already taken! Choose another number: ")
+            choice = int(choice)
     
 def is_player_starting():
-    if random.randomint(0, 1) == 0:
+    if random.randint(0, 1) == 0:
         print("Computer is starting")
         return False
     else:
@@ -33,26 +35,34 @@ def makeMove(board, letter, move):
     board[move] = letter
 
 def ai_move(board):
-    for i in range(1, 10):
+    for i in range(9):
         copy = getBoardCopy(board)
         if isSpaceFree(copy, i):
             makeMove(copy, "O", i)
             if is_game_over(copy):
                 makeMove(board, "O", i)
+                print("Ai choose " + str(i))
+                return board
             
-    for i in range(1, 10):
+    for i in range(9):
         copy = getBoardCopy(board)
         if isSpaceFree(copy, i):
             makeMove(copy, "X", i)
             if is_game_over(copy):
                 makeMove(board, "O", i)
+                print("Ai choose " + str(i))
+                return board
             
     if isSpaceFree(board, 5):
         makeMove(board, "O", 5)
+        print("Ai choose " + str(5))
+        return board
     
-    for i in range(1, 10):
+    for i in range(9):
         if isSpaceFree(board, i):
-           makeMove(board, "O", i)
+           makeMove(board, 'O', i)
+           print("Ai choose " + str(i))
+           return board
             
 
             
